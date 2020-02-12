3f57954e02732453467dec446f47c0172cc63cf3
brohlson <chase@chaseohlson.com>
2020-02-11 20:03:47 -0800

    deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    'gatsby-plugin-webpack-size',
     {
       resolve: 'gatsby-plugin-anchor-links',
       options: {
